6b20657fe222a4b1ece467c2d87b71d017feab87
Will Saffran <willh.saffran@gmail.com>
2019-02-05 02:19:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   if (phrase.upcase == phrase)
     puts "NO, NOT SINCE 1983!"
-  else !(phrase.upcase == phrase)
+  if !(phrase.upcase == phrase)
     puts "HUH?! SPEAK UP, SONNY!"
 end
